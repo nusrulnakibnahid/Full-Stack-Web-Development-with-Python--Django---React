1f470b40c44e35627026d3c632820bbd0a8ad24b
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-01-01 22:00:55 +0600

    new commit

--- DataTypes.py
+++ DataTypes.py
@@ -14,3 +14,75 @@ print(type(num1))
 num2 = 10.5
 print(num2)
 print(type(num2))
+
+
+#boolean data types
+is_true = True
+print(is_true)
+print(type(is_true))
+
+#complex data types
+num3 = 2 + 3j
+print(num3)
+print(type(num3))
+
+#list data types
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(type(fruits))
+
+
+#tuple data types
+coordinates = (10, 20)
+print(coordinates)
+print(type(coordinates))
+
+#dictionary data types
+person = {"name": "John", "age": 30}
+print(person)
+print(type(person))
+
+#set data types
+unique_numbers = {1, 2, 3, 4, 5}
+print(unique_numbers)
+print(type(unique_numbers))
+
+
+#none data types
+nothing = None
+print(nothing)
+print(type(nothing))
+
+
+
+#byte data types
+byte_data = b"Hello"
+print(byte_data)
+print(type(byte_data))
+
+
+#bytearray data types
+byte_array = bytearray(5)
+print(byte_array)
+print(type(byte_array))
+
+
+
+#frozenset data types
+frozen_set = frozenset([1, 2, 3, 4, 5])
+print(frozen_set)
+print(type(frozen_set))
+
+
+
+#range data types
+number_range = range(1, 10)
+print(number_range)
+print(type(number_range))
+
+
+#memoryview data types
+byte_array2 = bytearray(b"Hello")
+memory_view = memoryview(byte_array2)
+print(memory_view)
+print(type(memory_view))
