72b822f549edcd6eb1de979a8ed5214f6196e16e
nusrulnakibnahid <nakibnahid1054@gmail.com>
2026-01-07 15:02:56 +0600

    Add examples for accessing, changing, appending, and inserting items in a list

---
+++ Python/ListItem.py
@@ -0,0 +1,19 @@
+#Access List Item
+my_list = ['apple', 'banana', 'cherry', 'date']
+print(my_list[2])  
+
+
+#Change List Item
+my_list[1] = 'blueberry'
+print(my_list) 
+
+
+#Append List Item
+my_list.append(10)
+print(my_list)
+
+
+
+#Insert List Item
+my_list.insert(1, 'nahid')
+print(my_list)
