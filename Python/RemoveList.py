41be3fa1080a62c0c5db9dc66f0df6e44723ee6b
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-01-10 12:33:09 +0600

    Add examples for removing items from a list in Python

---
+++ Python/RemoveList.py
@@ -0,0 +1,21 @@
+#Remove Specified Item
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+thislist.remove("banana")
+print(thislist)
+
+#pop Specified Index
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+thislist.pop(2)
+print(thislist)
+
+
+#del Specified Index
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+del thislist[0]
+print(thislist)
+
+
+#clear All Items
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+thislist.clear()
+print(thislist)
