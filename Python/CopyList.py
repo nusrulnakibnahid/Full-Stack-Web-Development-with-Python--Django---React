d1ea3bdfcc8b55fd40bda2d60c1681b6c36a53e1
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-01-25 21:56:21 +0600

    Add example for copying a list in Python

---
+++ Python/CopyList.py
@@ -0,0 +1,18 @@
+# Simple way to copy a list in Python
+
+# Original list
+original_list = [1, 2, 3, 4, 5]
+
+# Copy the list
+copied_list = list(original_list)
+
+# Show the copy works
+print("Original:", original_list)
+print("Copy:", copied_list)
+
+# Change the copy
+copied_list.append(6)
+
+print("After change:")
+print("Original:", original_list)
+print("Copy:", copied_list)
