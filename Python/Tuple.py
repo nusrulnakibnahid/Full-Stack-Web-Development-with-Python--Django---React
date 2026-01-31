5a0e677545494543f19eb817da3da11f8169cb53
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-01-31 13:05:24 +0600

    Add examples for looping through tuples using for, range, and while loops

--- Python/Tuple.py
+++ Python/Tuple.py
@@ -62,3 +62,24 @@ fruits = ("apple", "banana", "cherry", "strawberry", "raspberry", "blueberry", "
 print(green)
 print(yellow)
 print(red)
+
+
+#loop Truple
+thistuple = ("apple", "banana", "cherry")
+for i in thistuple:
+    print(i)
+
+
+#Loop Through the Index Numbers
+thistuple = ("apple", "banana", "cherry")
+
+for i in range(len(thistuple)):
+    print(thistuple[i])
+
+
+#Using a While Loop
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+    print(thistuple[i])
+    i = i+1
