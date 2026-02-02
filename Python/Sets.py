e6f5b60ccd8efd92dd77e32fdfcf1e35a7ecf2e7
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-02 14:16:03 +0600

    Add examples demonstrating set creation, item access, and membership testing in Python

---
+++ Python/Sets.py
@@ -0,0 +1,12 @@
+MySet = {"apple", "banana", "cherry"}
+print(MySet)
+print(type(MySet))
+print(len(MySet))
+
+
+# Access Set Items
+MySet = {"apple", "banana", "cherry"}
+for i in MySet:
+    print(i)
+
+print ("apple" in MySet) # is apple present in the set (True or False)?
