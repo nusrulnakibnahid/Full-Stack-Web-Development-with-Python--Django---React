f83500b8ffa7e275554bd1c7594817b97019e80c
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-03 00:02:34 +0600

    Add examples for joining sets, intersection, difference, and symmetric difference in Python

--- Python/Sets.py
+++ Python/Sets.py
@@ -66,3 +66,63 @@ print(colors)  # Output: set()
 Set = {'a', 'b', 'c'}
 for i in Set:
     print(i)
+
+
+
+
+#Join Sets
+SetA = {'a', 'b', 'c'}
+SetB = {1, 2, 3}
+
+SetC = SetA.union(SetB)
+print(SetC)
+
+#Join multiple sets with the union() method
+Set1 = {'a', 'b', 'c'}
+Set2 = {1, 2, 3}
+Set3 = {True, False}
+Set4 = {'X', 'Y', 'Z'}
+
+Set5 = Set1.union(Set2, Set3, Set4)
+print(Set5)
+
+
+#Use | to join two sets
+
+A = {'a', 'b', 'c'}
+B = {1, 2, 3}
+
+C = A|B
+print(C)
+
+
+#Intersection of Sets
+SetX = {'a', 'b', 'c', 'd'}
+SetY = {'c', 'd', 'e', 'f'}
+
+SetZ = SetX.intersection(SetY)
+print(SetZ)
+
+# Difference (The difference() method will return a new set that will contain only the items from the first set that are not present in the other set.)
+
+numbers1 = {1, 2, 3, 4, 5}
+numbers2 = {4, 5, 6, 7, 8, 1, 2, 5, 4, 9}
+
+diff = numbers2.difference(numbers1)
+print(diff)  # Output: {6, 7, 8, 9} 
+
+
+# Symmetric Differences (The symmetric_difference() method will return a new set that will contain only the items that are not present in both sets.)
+X = {'a', 'b', 'c', 'd'}
+Y = {'c', 'd', 'e', 'f'}
+
+Z = X.symmetric_difference(Y)
+print(Z)
+
+
+
+
+
+#frozenset
+frozenSet = frozenset(['apple', 'banana', 'cherry'])
+print(frozenSet)
