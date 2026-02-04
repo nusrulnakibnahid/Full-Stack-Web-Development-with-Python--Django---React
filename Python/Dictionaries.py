a61d792717e980658483bebb3d63a1d49b0385fa
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-04 14:04:42 +0600

    Add examples demonstrating dictionary creation and item access in Python

---
+++ Python/Dictionaries.py
@@ -0,0 +1,22 @@
+MyName = {
+    "name" : "nahid",
+    "Age" : 24,
+    "City" : "Dhaka",
+}
+print(MyName)
+print(MyName["name"])
+print(type(MyName))
+
+
+# Another example
+
+StudentInfo = {
+
+    "NewStudent" : {
+    "name" : "nahid",
+    "Age" : 24,
+    "student ID" : 1054,
+    "faculty" : "SWE"
+    }
+}
+print(StudentInfo ["NewStudent"] ["Age"])
