7d0e83c2ab78efd547111be5692a311d70787d52
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-09 00:01:04 +0600

    Add examples for using if, else, and elif statements in Python

---
+++ Python/IfStatement.py
@@ -0,0 +1,35 @@
+# If Statement
+a = 50
+b = 20
+
+if a > b:
+    print('a is greater than b')
+
+
+# If...Else Statement
+x = 10
+y = 20
+
+if x>y:
+    print("x is greater than y")
+
+else:
+    print("x is less than y")
+
+
+
+#elif Statement
+
+num1 = 10
+num2 = 20
+
+if num1 > num2:
+    print("num1 is greater than num2")
+
+elif num1 < num2:
+    print("num1 is less than num2")
+
+elif num1 == num2:
+    print("num1 is equal to num2")
+else:
+    print("num1 and num2 are not comparable")
