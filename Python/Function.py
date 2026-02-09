f18940a0f83c2bb7906ccb1f100ec8272e67a042
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-09 23:50:04 +0600

    Add example function demonstrating basic arithmetic operations in Python

---
+++ Python/Function.py
@@ -0,0 +1,15 @@
+def example(a,b):
+	
+    sum = a + b 
+    print("Sum is:",sum)
+
+    sub = a - b
+    print("Sub is:",sub)
+
+    mul = a * b
+    print("Mul is:",mul)
+
+    div = a / b
+    print("Div is:",div)
+
+example(50,20)
