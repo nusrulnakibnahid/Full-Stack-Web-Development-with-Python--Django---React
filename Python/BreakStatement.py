f4ca964dbac55579ca45f006fef2d1f3e9211065
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-10 14:01:57 +0600

    Add examples demonstrating the use of break and continue statements in loops

---
+++ Python/BreakStatement.py
@@ -0,0 +1,15 @@
+# The break statement is used to exit a loop prematurely when a certain condition is met.
+for x in range(9):
+    if x == 5:
+        break
+    print(x)
+
+
+
+#another example of break statement
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in list:
+    if i == 6:
+        break
+    print(i)
---
+++ Python/ContinueStatement.py
@@ -0,0 +1,8 @@
+# Continue statement is used to skip the current iteration of a loop and move to the next iteration.
+
+name = ['nahid', 'rupanti', 'galib', 'arfa', 'nakib', 'mugdha']
+
+for i in name:
+    if i =='nakib':
+        continue
+    print(i)
