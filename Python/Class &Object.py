eeb9aae3e1c7f853772670fe0e56f663bfb238f3
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-11 11:36:26 +0600

    new commit

---
+++ Python/Class &Object.py
@@ -0,0 +1,26 @@
+class Student:
+    name = ""
+    roll = ""
+    city = ""
+    area = ""
+
+a = Student()
+a.name = "nahid"
+print(a.name)
+
+
+#another example
+
+class Stu:
+    Roll = ""
+    GPA = ""
+
+nahid = Stu()
+nahid.Roll = 1054
+nahid.GPA = 3.50
+print(f"Roll:{nahid.Roll} & GPA:{nahid.GPA}")
+
+nusrul = Stu()
+nusrul.Roll = 221
+nusrul.GPA = 4.00
+print(f"Roll:{nusrul.Roll} & GPA:{nusrul.GPA}")
