e57f54807d47553937377bd5cd50273d440a098e
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-11 18:18:29 +0600

    Add example demonstrating inheritance in Python

---
+++ Python/Inheritance.py
@@ -0,0 +1,82 @@
+class Baba:
+    car = "BMW"
+    taka = "100 cr"
+    home = "10th floor"
+
+
+class kaka(Baba):
+    Brokenphone =  ""
+    BrokenHome = ""
+
+
+
+k = kaka()
+print(k.car)
+
+
+
+#Multiple Inheritance
+class Father:
+    car = "BMW"
+    taka = "100 cr"
+    home = "10th floor"
+
+class Father2:
+    bike = "Yamaha"
+    money = "50 cr"
+    home = "5th floor"
+
+class Father3:
+    boat = "Yacht"
+    cash = "200 cr"
+    home = "20th floor"
+
+
+class father4:
+    land = "1000 acre"
+    cash = "500 cr"
+    home = "15th floor"
+
+
+class Uncle(Father, Father2, Father3, father4):
+    pass
+
+U = Uncle()
+print(U.land)
+print(U.home)
+print(U.bike)
+
+
+
+
+#Multilevel Inheritance
+class baba:
+    car = "BMW"
+    taka = "100 cr"
+    home = "10th floor"
+
+class son1(baba):
+    home = "5th floor"
+    phone = "iPhone"
+
+class son2(son1):
+    home = "20th floor"
+    bike = "Honda"
+
+class son3(son2):
+    area = "10 ac"
+    laptop = "ASUS"
+
+
+class son4(son3):
+    tab = ""
+    cycle = ""
+
+
+S = son4()
+print(S.laptop)
+print(S.car)
+
+
+
+    
