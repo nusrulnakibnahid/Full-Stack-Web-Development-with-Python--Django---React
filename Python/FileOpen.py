b3ae3bcc4498914296b2aebf8e5e2388c948f77a
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-13 22:50:06 +0600

    new commit

--- Python/FileOpen.py
+++ Python/FileOpen.py
@@ -1,7 +1,25 @@
-f = open("demofile.txt", "r")
+
+f = open("demofile.txt","r")
 
 print(f.read())
 
 
+# File Write 
 
-# Python File Write 
+newFile = open("index.html", "a")
+
+newFile.write("<h1> Hello World </h1>")
+
+print(newFile.read())
+
+
+
+
+# Delete File
+
+import os
+os.remove("index.html")
+
+# create a folder then delete it
+os.mkdir("myfolder")
+os.rmdir("myfolder")
