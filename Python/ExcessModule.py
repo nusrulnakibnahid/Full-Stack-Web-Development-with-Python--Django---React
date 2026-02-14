d57f59ee42771588f3f6eec022ba2da15692ad53
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-14 13:56:48 +0600

    new commit

---
+++ Python/ExcessModule.py
@@ -0,0 +1,15 @@
+import Module
+Module.name()
+
+import Module
+Module.nahid()
+
+# change Module.py file and run this file again to see the changes.
+import Module as Mod
+Mod.name()
+
+
+
+import Module
+x = Module.person1
+print(x)
---
+++ Python/Module.py
@@ -0,0 +1,15 @@
+def name():
+    print("Nahid is sleeping")
+
+
+
+def nahid():
+   print("He is a Software Engineer")
+
+
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
