5ed52a654da030d4be9594e8753cd9c07952401c
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-15 11:59:37 +0600

    new commit

---
+++ Python/OOP.py
@@ -0,0 +1,8 @@
+# Parameterized Constructor
+
+class parentInfo:
+    def NahidFamily(self,name,age):
+
+        print(f"my name is {name} and my age is {age}")
+p1 = parentInfo()
+p1.NahidFamily("Nahid", 25)
