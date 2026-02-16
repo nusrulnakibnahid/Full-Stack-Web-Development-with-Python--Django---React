019203e0f5a7e7aea342be522665a06ef5dd6d50
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-16 22:41:29 +0600

    new commit

--- Python/OOP.py
+++ Python/OOP.py
@@ -1,4 +1,4 @@
-# Parameterized Constructor
+# Constructor
 
 class parentInfo:
     def NahidFamily(self,name,age):
@@ -7,3 +7,13 @@ class parentInfo:
 p1 = parentInfo()
 p1.NahidFamily("Nahid", 25)  
 p1.NahidFamily("Nakib", 30)
+
+
+# Parameterized Constructor
+class StudentInfo:
+    def __init__(self,name,num):
+        
+        print(f"My name is {name} and my id number is {num}")
+
+        s1 = StudentInfo("Nahid","1054")
+
