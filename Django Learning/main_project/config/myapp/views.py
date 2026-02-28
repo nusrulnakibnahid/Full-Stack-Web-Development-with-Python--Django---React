fd39ddcaed4d69a0b3e53df1ce5724e663e47c65
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-28 15:00:12 +0600

    Add views and URLs for myapp, including about page template

--- Django Learning/main_project/config/myapp/views.py
+++ Django Learning/main_project/config/myapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello Django, I am Nahid")
+
+
+def profile(request):
+    return HttpResponse("This is my profile page")
+
+
+def dashboard(request):
+    return HttpResponse("This is my dashboard page")
+
+
+
+def about(request):
+    return render(request, 'about.html')
